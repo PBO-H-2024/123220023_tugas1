a1e22fef93f3a2505d924c76ac53ac89eb1958bc
Nofangagah <zohrialpoison@gmail.com>
2024-05-16 19:15:48 +0700

    add assigment mvc

---
+++ assigment2/pertemuan6/src/App.java
@@ -0,0 +1,16 @@
+
+import Controller.Controller;
+import View.*;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new Read(new Controller());
+        
+
+     
+   
+      
+    }
+}
